43fd94cd133351096ed94adac1cb2e5be7907b71
Sudhichai_Ungsuthornrungsi <oah.s@excelbangkok.com>
2016-10-03 18:41:49 +0700

    Update code

--- stopwatch/stopwatch/Program.cs
+++ stopwatch/stopwatch/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,17 @@ namespace stopwatch
 {
     class Program
     {
-        static void Main(string[] args)
+     static void Main(string[] args)
         {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+         
+
+            Console.ReadKey();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Duration());
+            Console.ReadKey();
+
         }
     }
 }
---
+++ stopwatch/stopwatch/Stopwatch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace stopwatch
+{
+    public class Stopwatch
+    {
+        private TimeSpan duration { get; set; }
+        private readonly DateTime startTime = new DateTime();
+        private DateTime stopTime { get; set; }
+
+        
+
+        public void Start()
+        {
+            this.startTime = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            this.stopTime = DateTime.Now;
+        }
+
+        public TimeSpan Duration()
+        {
+            this.duration = stopTime.Subtract(startTime);
+            return this.duration;
+        }
+
+    }
+}
